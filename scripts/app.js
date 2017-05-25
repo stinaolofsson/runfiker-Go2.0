e4119de45d4a7d7ace2240c0f7056c5b6e986064
stinaolofsson <stina.e.olofsson@live.se>
2017-05-25 19:16:24 +0200

    Google map with current position

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+angular.module('runFikerGo', ['ngMap'])
+  .controller('runFikerController', function($http, $scope){
+    // Show and view scopes
+    $scope.chatView = false;
+    $scope.mapView =true;
+  });
---
+++ scripts/map.js
@@ -0,0 +1,19 @@
+angular.module('myMap',['ngMap']).controller('MyCtrl', function(NgMap) {
+    var vm = this;
+    vm.message = 'You can not hide. :)';
+    NgMap.getMap("map").then(function(map) {
+      vm.map = map;
+    });
+    vm.callbackFunc = function(param) {
+      console.log('I know where '+ param +' are. ' + vm.message);
+      console.log('You are at' + vm.map.getCenter());
+    };
+    $scope.mouseover = function() {
+      console.log('mouseover', this);
+      this.style.backgroundColor = 'grey';
+    };
+    $scope.mouseout = function() {
+      this.style.backgroundColor = 'white';
+    };
+    $scope.click = function() {console.log('click')};
+  });
